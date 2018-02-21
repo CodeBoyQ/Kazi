d6e0128e47085c76a3478e19e6de9c6651d58de5
unknown <codeboy@arkitek.com>
2018-02-21 22:35:29 +0100

    Initial commit

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+Tutorial: https://spring.io/guides/gs/actuator-service/#scratch
+
+Build:
+mvn clean install
+
+Run:
+java -jar target/Kazi-0.0.1-SNAPSHOT.jar
+
+Test:
+http://localhost:8080/kazi/hello-world
---
+++ src/main/java/com/arkitek/nova/KaziConfiguration.java
@@ -0,0 +1,13 @@
+package com.arkitek.nova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaziConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaziConfiguration.class, args);
+    }
+
+}
