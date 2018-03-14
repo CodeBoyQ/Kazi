cc2bbf0b011058b800fd7a51cad723ed71a87aba
CodeboyQ <codeboyq@wiz.com>
2018-03-14 17:45:19 +0100

    Removed target directory and added it to gitignore

--- src/main/java/com/arkitek/nova/KaziConfiguration.java
+++ src/main/java/com/arkitek/nova/KaziConfiguration.java
@@ -3,6 +3,12 @@ package com.arkitek.nova;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application Kazi
+ * Copyright (c) 2018
+ * @author astronaut
+ *
+ */
 @SpringBootApplication
 public class KaziConfiguration {
 
